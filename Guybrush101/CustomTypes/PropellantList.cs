8723af4593e04c961d598fe29187080a101b8841
Ulrik Kofod <webshop@kofod.co.uk>
2016-03-30 22:46:37 +0200

    Initial

---
+++ Guybrush101/CustomTypes/PropellantList.cs
@@ -0,0 +1,148 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using UnityEngine;
+using Guybrush101.GenericFunctions;
+
+namespace Guybrush101.CustomTypes
+{
+    public class PropellantList
+    {
+        //private string _names;            //New name of part in event of new propellant
+        private string _propellants;        //The propellants
+        private int _propAmount;
+        private string _propRatios;         //The propellant ratios
+        private float _propDensity;
+        private string _ignoreForIsp;
+        private string _atmosphereCurve;
+        private string _velCurve;
+        private string _atmCurve;
+
+
+        //find ISP Turbine = 1 atm, LiquidFuel = 0 atm and use the function for FuelFlow to ensure right flow rate
+
+        private float _maxThrust;           //  Kg * m/s^2 = N
+        //private float _maxFuelFlow;         //  Kg/s
+
+        //For storing and retrieving propellants
+        public string Propellants
+        {
+            get { return _propellants; }
+            set { _propellants = value; CalcDensity(_propellants, _propRatios); }
+        }
+
+        //For storing and retrieving propellant ratios
+        public string PropRatios
+        {
+            get { return _propRatios; }
+            set
+            {
+                string[] arrInString;
+                bool booparse;
+
+                _propRatios = value;
+                
+                //Evaluate if multi propellants are in the string, put result to _propAmount
+                arrInString = value.Split(',');
+                _propAmount = arrInString.Length;
+                try
+                {
+                    foreach(string item in arrInString)
+                    {
+                        float numvalue;
+                        booparse = Single.TryParse(item, out numvalue);
+                        if (!booparse)
+                                Debug.LogWarning("CustomTypes.PropellantList -> Could not parse propellant ratio " + item + " into integer.");
+                    }
+                }
+                catch (Exception e) { Debug.LogError("CustomTypes.PropellantList -> Could not parse propellant ratio into integer.\n" + value + "\nError trown:\n" + e); }
+                CalcDensity(_propellants,_propRatios);
+            }
+        }
+        public float propDensity
+        {
+            get { return _propDensity; }
+        }
+        private bool CalcDensity(string inPropellants, string inRatios)
+        {
+            bool returnSuccessStatus = false;
+            string[] arrInPropellants, arrInRatios;
+            try
+            {
+                arrInPropellants = inPropellants.Trim().Split(',');
+                arrInRatios = inRatios.Trim().Split(',');
+                if (arrInPropellants.Length != arrInRatios.Length) { return false; }
+            }
+            catch
+            {
+                //If split fails, return no success
+                return false;
+            }
+
+            try
+            { 
+                if (arrInPropellants.Length == arrInRatios.Length)
+                {
+                    EngineCalculations fx = new EngineCalculations();
+                    Debug.Log("Running _propDensity = fx.calcWeightedDensity(_propellants, _propRatios)");
+                    _propDensity = fx.calcWeightedDensity(_propellants, _propRatios);
+                    if ( _propDensity > 0 ) { returnSuccessStatus = true; } else { returnSuccessStatus = false; }
+                    Debug.Log("_propDensity = fx.calcWeightedDensity(_propellants, _propRatios) is successfull");
+                }
+            }
+            catch
+            {
+                Debug.LogError("Guybrush101.CustomTypes.CalcDensity Failed By Exception");
+                returnSuccessStatus = false;
+                //throw;
+            }
+
+            return returnSuccessStatus;
+        }
+        //public void setMaxThrust(string inMaxThrust)
+        //{
+        //    float outMaxThrust = 0;
+        //    Single.TryParse(inMaxThrust, out outMaxThrust);
+        //    _maxThrust = outMaxThrust;
+        //}
+        public string setMaxThrust
+        {
+            set
+            {
+                float outMaxThrust = 0;
+                Single.TryParse(value, out outMaxThrust);
+                _maxThrust = outMaxThrust;
+            }
+        }
+        public float maxThrust
+        {
+            get { return _maxThrust; }
+        }
+        public string propIgnoreForISP
+        {
+            get { return _ignoreForIsp; }
+            set { _ignoreForIsp = value; }
+        }
+        public int propAmount
+        {
+            get { return _propAmount; }
+            //set { _propAmount = value; }  //Internally calculated
+        }
+        public string atmosphereCurve
+        {
+            get { return _atmosphereCurve; }
+            set { _atmosphereCurve = value; }
+        }
+        public string velCurve
+        {
+            get { return _velCurve; }
+            set { _velCurve = value; }
+        }
+        public string atmCurve
+        {
+            get { return _atmCurve; }
+            set { _atmCurve = value; }
+        }
+    }
+}
---
+++ Guybrush101/G101.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using UnityEngine;
+
+namespace Guybrush101
+{
+    /// <summary>
+    /// My first part!
+    /// </summary>
+    public class PropulsionTech : PartModule
+    {
+        //private List<ModuleEngines> ModuleEngines;
+        //[KSPField(isPersistant = true)]
+        //float originalThrust = 0;
+        [KSPField]
+        public string engineID;
+
+
+        [KSPField(isPersistant = true)]
+        bool booToggle = false;
+        private string info;
+
+        float Thrustpct = 50;
+        float MaxThrust = 100;
+        float maxFuelFlow = 0;
+
+        private ModuleEngines engineModule;
+        private List<ModuleEngines> ModuleEngines;
+
+        [KSPEvent(active = true, guiActive = true, guiActiveEditor = true, guiName = "THRUST")]
+        public void toggleupdate()
+        {
+            //Manipulate this parent part of the module | this.part.####
+            //Get the engines module of the part
+            ModuleEngines engineModule = PropulsionTech.getEnginesModule(this.part);
+
+            //MaxThrust = engineModule.maxThrust;
+            
+            MaxThrust = ToggleMaxThrust(engineModule, Thrustpct, ref booToggle);
+            //engineModule.propellants.
+
+            //Inverse the boolean indicator
+            //booToggle = !booToggle;
+
+            //#### DEBUG ####
+            info = "Burst toggleupdate\n";
+            info += "MaxThrust " + MaxThrust + "\n";
+            info += "maxFuelFlow " + maxFuelFlow + "\n";
+            info += "booToggle " + booToggle;
+            Debug.Log(info);
+            //#### DEBUG ####
+
+            //Update the event text
+            Events["toggleupdate"].guiName = (booToggle ? "MaxThrust (burst) " + MaxThrust : "MaxThrust (base) " + MaxThrust);
+
+        }
+
+        //This function sets/unsets burst, and returns the new thrust value
+        private float ToggleMaxThrust(ModuleEngines module, float Thrustpct, ref bool currentThrustState)
+        {
+            if (currentThrustState == true)
+            {
+                //i.e. if burst, then return the engine to normal state
+                module.maxThrust = module.maxThrust / (1 + Thrustpct / 100);
+                module.maxFuelFlow = module.maxFuelFlow / (1 + Thrustpct / 100);
+                maxFuelFlow = module.maxFuelFlow;
+                currentThrustState = false;                   //true = burst | false = normal
+            }
+            else
+            {
+                //i.e. if not burst, then burst the engine
+                module.maxThrust = module.maxThrust * (1 + Thrustpct / 100);
+                module.maxFuelFlow = module.maxFuelFlow * (1 + Thrustpct / 100);
+                maxFuelFlow = module.maxFuelFlow;
+                currentThrustState = true;
+            }
+            return module.maxThrust;
+        }
+        private void StartBurst(ModuleEngines module, float Thrustpct, ref bool currentThrustState)
+        {
+
+        }
+        private void StopBurst(ModuleEngines module, float Thrustpct, ref bool currentThrustState)
+        {
+
+        }
+
+        public static ModuleEngines getEnginesModule(Part part)
+        {
+            foreach (PartModule partModule in part.Modules)
+            {
+                if (partModule.moduleName == "ModuleEngines" || partModule.moduleName == "ModuleEnginesFX") return (ModuleEngines)partModule;
+            }
+            return null;
+        }
+
+        //Initialize the event text based on the input
+        public override void OnStart(PartModule.StartState state)
+        {
+            ModuleEngines engineModule = PropulsionTech.getEnginesModule(this.part);
+            MaxThrust = engineModule.maxThrust;
+
+            //#### DEBUG ####
+            //info = "Burst OnStart\n";
+            //info += "MaxThrust " + MaxThrust + "\n";
+            //info += "maxFuelFlow " + maxFuelFlow + "\n";
+            //info += "booToggle " + booToggle;
+            //Debug.LogError(info);
+            //#### DEBUG ####
+
+            //                                                  true                                false
+            Events["toggleupdate"].guiName = (booToggle ? "MaxThrust (burst) " + MaxThrust : "MaxThrust (base) " + MaxThrust);
+        }
+
+        ////Infomation
+        public override string GetInfo()
+        {
+            if (info == null)
+            {
+                info = "Burst\n";
+                info += "MaxThrust " + MaxThrust + "\n";
+                info += "maxFuelFlow " + maxFuelFlow + "\n";
+                info += "booToggle " + booToggle;
+            }
+            return info;
+        }
+
+
+
+        /// <summary>
+        /// Called when the part is started by Unity.
+        /// </summary>
+        //public override void OnStart(StartState state)
+        //{
+        //    // Add stuff to the log
+        //    //print("Hello, Kerbin!");
+        //    //ModuleEngines = part.FindModulesImplementing<ModuleEngines>();
+        //    //ModuleEngines = part.FindModulesImplementing<ModuleEngines>();
+
+        //    //foreach (var moduleEngine in ModuleEngines)
+        //    //{
+        //    //    //if (moduleEngine.engineID == engineID)  // || engineID.IsNullOrWhiteSpace())
+        //    //    if (moduleEngine.engineID == "")
+        //    //    {
+
+        //    //    }
+        //    //}
+        //}
+    }
+}
