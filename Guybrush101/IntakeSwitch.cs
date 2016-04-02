2fc72a5f0fc1ab4696dc26cb0ddbb6b3df387b4f
Ulrik Kofod <webshop@kofod.co.uk>
2016-04-03 00:44:11 +0200

    Introduction of intake switch, first elements are done.

---
+++ Guybrush101/IntakeSwitch.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using GTI.GenericFunctions;
+
+namespace GTI
+{
+    partial class GTI_IntakeSwitch : PartModule
+    {
+        /* Example on an Intake Module
+        name = ModuleResourceIntake
+        resourceName = IntakeAir
+
+        checkForOxygen = true
+		area = 0.0031
+		intakeSpeed = 15
+		intakeTransformName = Intake
+        machCurve
+        {
+            key = 1 1 0 0
+			key = 1.5 0.9 -0.4312553 -0.4312553
+			key = 2.5 0.45 -0.5275364 -0.5275364
+			key = 3.5 0.1 0 0
+		}
+        */
+
+        [KSPField]
+        public string resourceNames = "IntakeAir, IntakeAtm";
+        [KSPField]
+        public string areas = string.Empty;                      //"0.0031, 0.0031";
+        [KSPField]
+        public string intakeSpeeds = string.Empty;              //"15, 15";
+        [KSPField]
+        public string intakeTransformNames = string.Empty;       //"Intake, Intake";
+        [KSPField]
+        public string machCurves = string.Empty;
+
+
+        [KSPField(isPersistant = true)]
+        public int selectedIntake = -1;
+
+
+        #region Booleans for existence checks
+        private bool resourceNamesEmpty = false;
+
+        #endregion
+
+        #region Other class level declarations
+        private bool _settingsInitialized = false;
+
+        private string[] arrIntakeNames;
+
+        private List<ModuleResourceIntake> ModuleIntakes;
+
+        #endregion
+
+        #region Events
+        public override void OnStart(PartModule.StartState state)
+        {
+            InitializeSettings();
+            if (selectedIntake == -1)
+            {
+                selectedIntake = 0;
+            }
+            updateIntake(false, "OnStart");
+        }
+
+        private void InitializeSettings()
+        {
+            if (!_settingsInitialized)
+            {
+                #region GUI Update
+                var nextEvent = Events["nextIntakeEvent"];
+                nextEvent.guiActive = availableInFlight;
+                nextEvent.guiActiveEditor = availableInEditor;
+
+                var previousEvent = Events["previousIntakeEvent"];
+                previousEvent.guiActive = availableInFlight;
+                previousEvent.guiActiveEditor = availableInEditor;
+                #endregion
+
+                #region Parse Arrays
+                arrIntakeNames = resourceNames.Trim().Split(';');
+
+
+
+                #endregion
+
+                if (string.IsNullOrEmpty(resourceNames) || resourceNames.Trim().Length == 0)
+                {
+                    Debug.LogError("GTI_IntakeSwitch is missing settings for intake names.");
+                    return;
+                }
+
+
+                #region Check Input existance etc.
+                resourceNamesEmpty = ((string.IsNullOrEmpty(resourceNames) || resourceNames.Trim().Length == 0));
+                #endregion
+
+                #region Check and split into arrays
+                if (!resourceNamesEmpty)
+                {
+                    arrIntakeNames = resourceNames.Trim().Split(';');
+                }
+                #endregion
+
+                ModuleIntakes = part.FindModulesImplementing<ModuleResourceIntake>();
+                //foreach (PartModule moduleIntake in ModuleIntakes)
+                //{
+                //}
+
+                //set settings to initialized
+                _settingsInitialized = true;
+
+            }
+        }
+        #endregion
+
+        #region UpdatePart Intake Module
+        private void updateIntake(bool calledByPlayer, string callingFunction = "player")
+        {
+            foreach (PartModule moduleIntake in ModuleIntakes)
+            {
+                //*****************************
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ Guybrush101/IntakeSwitch_UI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using GTI.GenericFunctions;
+
+namespace GTI
+{
+    partial class GTI_IntakeSwitch : PartModule
+    {
+        #region KSPFields and supporting settings
+
+        //Availability of the functionality
+        [KSPField]
+        public bool availableInFlight = false;
+        [KSPField]
+        public bool availableInEditor = true;
+        #endregion
+
+        #region User_Interface
+        //START - Events for selection of propellants
+        //NEXT
+        [KSPEvent(guiActive = false, guiActiveEditor = false, guiName = "Next propellant setup")]
+        public void nextIntakeEvent()
+        {
+            //InitializeSettings();
+            selectedIntake++;
+            if (selectedIntake > arrIntakeNames.GetUpperBound(0))
+            {
+                //if we move from last propellant, then the next one is the first one - aka 0
+                selectedIntake = 0;
+            }
+            updateIntake(true);
+        }
+        //PREVIOUS
+        [KSPEvent(guiActive = false, guiActiveEditor = false, guiName = "Previous propellant setup")]
+        public void previousIntakeEvent()
+        {
+            //InitializeSettings();
+            selectedIntake--;
+            if (selectedIntake < 0)
+            {
+                //if we move from the first propellant, then the previous is the last - aka the upperbound
+                selectedIntake = arrIntakeNames.GetUpperBound(0);
+            }
+            updateIntake(true);
+        }
+        //END - Events for selection of propellants
+        #endregion
+
+        [KSPAction("Next Intake")]
+        public void nextIntakeAction(KSPActionParam param)
+        {
+            nextIntakeEvent();
+        }
+        [KSPAction("Previous Intake")]
+        public void previousIntakeAction(KSPActionParam param)
+        {
+            previousIntakeEvent();
+        }
+    }
+}
